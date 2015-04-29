01d96e6eed20c8c1e174189d7ba4052f5aa5c071
Christian Reichel <reichel.christian@gmail.com>
2015-04-29 16:47:43 -0300

    Adicionado form sample

--- src/main/java/br/tur/reservafacil/piador/SimpleServlet.java
+++ src/main/java/br/tur/reservafacil/piador/SimpleServlet.java
@@ -3,6 +3,7 @@ package br.tur.reservafacil.piador;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,13 +18,31 @@ public class SimpleServlet extends HttpServlet {
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
                     throws ServletException, IOException {
         logger.warn("Recebido GET");
-        response.getWriter().print("Hello World via GET");
+        final PrintWriter out = response.getWriter();
+        out.println("<HTML><BODY>");
+        out.println("<BR><BR>");
+        out.println("info:");
+        out.println("<BR><BR>");
+        out.println("<H2>metod GET</H2>");
+        out.println("<BR><BR>");
+        out.println("SERVER_NAME="+request.getServerName()+"<BR>");
+        out.println("REQUEST_METHOD="+request.getMethod()+"<BR>");
+        out.println("QUERY_STRING="+request.getQueryString()+"<BR>");
+        out.println("REMOTE_HOST="+request.getRemoteHost()+"<BR>");
+        out.println("REMOTE_ADDR="+request.getRemoteAddr());
+        out.println("</BODY></HTML>");
     }
 
     @Override protected void doPost(HttpServletRequest request, HttpServletResponse response)
                     throws ServletException, IOException {
         logger.warn("Recebido POST");
-        response.getWriter().print("Hello via POST");
+        final String username = request.getParameter("username");
+        final String password = request.getParameter("password");
+        logger.warn("Login: " + username);
+        logger.warn("Senha: " + password);
+        response.getWriter().println("Hello via POST");
+        response.getWriter().println("Login: " + username);
+        response.getWriter().println("Senha: " + password);
         response.getWriter().flush();
     }
 }
--- src/main/webapp/hello.jsp
+++ src/main/webapp/hello.jsp
@@ -8,7 +8,24 @@
 </head>
 <body>
 <div class="container">
-=)
+    <H1>Hello</H1>
+    <hr/>
+    <form class="form-inline" method="post" action="SimpleServlet">
+      <div class="form-group">
+        <label class="sr-only" for="exampleInputEmail3">Email address</label>
+        <input name="username" type="text" class="form-control" id="exampleInputEmail3" placeholder="Enter email">
+      </div>
+      <div class="form-group">
+        <label class="sr-only" for="exampleInputPassword3">Password</label>
+        <input name="password" type="password" class="form-control" id="exampleInputPassword3" placeholder="Password">
+      </div>
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </form>
 </div>
 </body>
 </html>

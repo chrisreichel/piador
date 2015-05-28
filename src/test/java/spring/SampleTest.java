5a1b201292d83668a20ee43f8e2e3d9a8f6875c0
Christian Reichel <reichel.christian@gmail.com>
2015-05-28 14:15:21 -0300

    Adicionando suporte a transações

--- src/test/java/spring/SampleTest.java
+++ src/test/java/spring/SampleTest.java
@@ -11,6 +11,7 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
